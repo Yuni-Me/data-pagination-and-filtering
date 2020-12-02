8f883b7be7607fe0296170f5b9a8920f3c999907
Yuni Mata <7897746+Yuni-Me@users.noreply.github.com>
2020-12-02 11:55:59 -0800

    Remove the pagination button if result is less than a page

--- js/script.js
+++ js/script.js
@@ -23,7 +23,7 @@ function showPage(list, page) {
    const endIndex = page * itemsPerPage;
    const studentList = document.querySelector('.student-list');
    studentList.innerHTML = '';
-   
+
    // create a conditional to display "No results" when search yields 0 results
    if (list.length === 0) {
       studentList.innerHTML = '<h1>No Results Found</h1>';
@@ -43,7 +43,7 @@ function showPage(list, page) {
                               </div>
                            </li>`;
             studentList.insertAdjacentHTML('beforeend', student);
-               
+
          }
       }
    }
@@ -57,11 +57,14 @@ function addPagination(list) {
    const pagination = document.querySelector('.link-list');
    pagination.innerHTML = '';
 
-   if(list.length !== 0) {
-      // loop over the number of pages needed 
+   // create a conditional to remove the pagination buttons entirely if there is only one // page of results
+   if (list.length < 10) {
+      pagination.innerText = '';
+   } else {
+      // loop over the number of pages needed
       for (let i = 1; i <= numOfPages; i++) {
          // Create the button elements
-         const button =   `<li>
+         const button = `<li>
                            <button type="button">${i}</button>
                         </li>`;
          pagination.insertAdjacentHTML('beforeend', button);
@@ -69,7 +72,9 @@ function addPagination(list) {
       // Give the first button a class of "active"
       const firstButton = document.querySelector("li button");
       firstButton.classList.add('active');
-   }  
+   }
+      
+   // }
    // Create an event listener on the "link-list" element
    pagination.addEventListener('click', (e) => {
       if (e.target.tagName === 'BUTTON') {
@@ -86,7 +91,7 @@ function addPagination(list) {
 /*
 This function will filter students and display results based on search criteria
 */
-function filterStudents(searchString){
+function filterStudents(searchString) {
    let newData = [];
    for (let i = 0; i < data.length; i++) {
       const name = `${data[i].name.first} ${data[i].name.last}`
@@ -100,7 +105,7 @@ function filterStudents(searchString){
 /*
 This function will handle the 'keyup' and 'click' type event listeners
 */
-function handleSearchEvent(){
+function handleSearchEvent() {
    const newData = filterStudents(searchValue);
    showPage(newData, 1);
    addPagination(newData);
@@ -116,5 +121,4 @@ addPagination(data);
 Event Listeners
 */
 searchValue.addEventListener('keyup', handleSearchEvent);
-
 searchButton.addEventListener('click', handleSearchEvent);
